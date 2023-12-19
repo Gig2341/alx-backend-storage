c16d627411b2b494811bcd280ccb4d2de7cbff64
Gig2341 <danmata2341@gmail.com>
2023-12-19 00:02:07 +0000

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+'''
+this module lists all documents in a collection
+returns an empty list if no document in the collection
+'''
+
+
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    '''a function that lists all documents in a collection'''
+    for document in mongo_collection.find():
+        return document
